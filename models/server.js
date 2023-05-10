d039249244879352c727422cd2ccde9d50953943
sergio <sheko3857@gmail.com>
2023-05-10 01:04:12 -0700

    practicando crud

--- models/server.js
+++ models/server.js
@@ -26,7 +26,31 @@ class Server {
 
     routes(){
         this.app.get('/api',  (req, res)=> {
-            res.send('Hello World');
+            res.json({
+                
+                msg:"get API"
+            });
+
+            this.app.put('/api',  (req, res)=> {
+                res.json({
+                    
+                    msg:"put API"
+                });
+              })
+
+              this.app.post('/api',  (req, res)=> {
+                res.status(201).json({
+                    
+                    msg:"post API"
+                });
+              })
+
+              this.app.delete('/api',  (req, res)=> {
+                res.json({
+                    
+                    msg:"delete API"
+                });
+              })
           })
           
     }
