80bda6550952fd2204a587307dcffc83bb147c19
sergio <sheko3857@gmail.com>
2023-06-12 21:23:29 -0700

    terminado crud de buscar

--- models/server.js
+++ models/server.js
@@ -13,6 +13,7 @@ class Server {
 
         this.paths = {
             auth: "/api/auth",
+            buscar:"/api/buscar",
             categorias:"/api/categorias",
             productos:"/api/productos",
             usuarios:"/api/usuarios",
@@ -56,9 +57,11 @@ class Server {
 
     routes(){
         this.app.use(this.paths.auth, require("../routes/auth"))
+        this.app.use(this.paths.buscar,require("../routes/buscar"))
         this.app.use(this.paths.usuarios, require("../routes/user"))
         this.app.use(this.paths.categorias,require("../routes/categorias"))
         this.app.use(this.paths.productos,require("../routes/productos"))
+       
           
     }
 
---
+++ routes/buscar.js
@@ -0,0 +1,10 @@
+const {Router} = require("express");
+const { buscar } = require("../controller/buscar");
+
+
+
+const router = Router();
+
+    router.get("/:coleccion/:termino",buscar)
+
+module.exports = router;
