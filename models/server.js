073182474f63e2fdb32a4132292b7b01e3309a42
sergio <sheko3857@gmail.com>
2023-06-14 12:48:17 -0700

    uploads,en proceso

--- models/server.js
+++ models/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require("cors");
 const { dbConnection } = require('../db/config');
+const fileUpload = require("express-fileupload");
 
 
 class Server {
@@ -17,6 +18,7 @@ class Server {
             categorias:"/api/categorias",
             productos:"/api/productos",
             usuarios:"/api/usuarios",
+            uploads:"/api/uploads",
            
            
 
@@ -51,6 +53,10 @@ class Server {
         //Directorio publico
         this.app.use(express.static("public"))
         //
+        this.app.use(fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/tmp/'
+        }));
         
     }
 
@@ -61,6 +67,7 @@ class Server {
         this.app.use(this.paths.usuarios, require("../routes/user"))
         this.app.use(this.paths.categorias,require("../routes/categorias"))
         this.app.use(this.paths.productos,require("../routes/productos"))
+        this.app.use(this.paths.uploads,require("../routes/uploads"))
        
           
     }
--- routes/productos.js
+++ routes/productos.js
@@ -1,3 +1,4 @@
+
 const {Router} = require("express");
 const { check } = require("express-validator");
 
