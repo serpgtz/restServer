076414408b176cc9f286bf7b7782d5da05fe06de
sergio <sheko3857@gmail.com>
2023-06-16 07:01:12 -0700

    actualizar imagen de usuario

--- controller/uploads.js
+++ controller/uploads.js
@@ -3,6 +3,9 @@
 const { response } = require("express");
 const { subirArchivo } = require("../helpers")
 
+const {Usuario, Producto} = require("../models/");
+const { model } = require("mongoose");
+
 
 
 const cargarArchivo = async(req,res=response) => {
@@ -15,7 +18,7 @@ const cargarArchivo = async(req,res=response) => {
 
     try {
         //  const nombre = await subirArchivo(req.files,["txt"],"textos"); aqui un ejemplo como le paso que formato quiero que acepte y el nombre de la carperta en donde quiero que se guarden
-         const nombre = await subirArchivo(req.files);
+         const nombre = await subirArchivo(req.files,undefined,"imgs");
 
          res.json({
          nombre
@@ -36,10 +39,45 @@ const actualizarImagen = async( req, res=response ) => {
 
         const {id, coleccion} = req.params;
 
-        res.json({
-            id,
-            coleccion
-        })
+
+        let modelo;
+    
+
+        switch (coleccion) {
+            case "usuarios":
+
+                modelo = await Usuario.findById(id);
+                if( !modelo ){
+                    return res.status(400).json({
+                        msg:`No existe el usuario con el id ${id}`
+                    })
+                }
+                
+                break;
+            case "productos":
+
+                modelo = await Producto.findById(id);
+                if( !modelo ){
+                    return res.status(400).json({
+                        msg:`No existe Producto con el id ${id}`
+                    })
+                }
+                
+                break;
+        
+            default:
+                return res.status(500).json({msg:"No existe validacion para esta opcion"})
+                
+        }
+
+        const nombre = await subirArchivo(req.files,undefined,coleccion);
+        modelo.img = nombre;
+
+        await modelo.save();
+
+
+
+        res.json(modelo)
 
 
 }
